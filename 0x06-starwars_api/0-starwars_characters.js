3610f762d6c672848a37d0fb6837669740fa2f0c
calvean <garnetsharara@gmail.com>
2023-05-30 23:18:15 +0200

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,39 +1,43 @@
 #!/usr/bin/node
 
 const request = require('request');
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
-function getCharacters (movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
+request(url, async (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-      console.error('Status:', response.statusCode);
-    } else {
-      const film = JSON.parse(body);
-      console.log(`Characters in "${film.title}":`);
+  const characters = JSON.parse(body).characters;
+  const characterSet = new Set();
 
-      film.characters.forEach((characterUrl) => {
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            console.error('Error:', error);
-          } else if (response.statusCode !== 200) {
-            console.error('Status:', response.statusCode);
-          } else {
-            const character = JSON.parse(body);
-            console.log(character.name);
-          }
-        });
-      });
+  for (const characterUrl of characters) {
+    try {
+      const characterData = await fetchCharacterData(characterUrl);
+      const characterName = characterData.name;
+
+      if (!characterSet.has(characterName)) {
+        console.log(characterName);
+        characterSet.add(characterName);
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
+});
+
+function fetchCharacterData (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(new Error(`Request failed with status code ${response.statusCode}`));
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
   });
-}
-
-const movieId = process.argv[2];
-
-if (movieId) {
-  getCharacters(movieId);
-} else {
-  console.error('Please provide a Movie ID.');
 }
