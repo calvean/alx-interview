e0a9106b6526a8ac9c93c74cbb9386e81e068ab9
calvean <garnetsharara@gmail.com>
2023-06-08 17:53:57 +0200

    fixed shebang

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ rotate 2d matrix module """
-from typing import List
 
 
-def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix 90 degrees clockwise in-place.
 
