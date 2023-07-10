7172e5d45d122c5201765d0c211b9a05b594902e
calvean <garnetsharara@gmail.com>
2023-07-10 16:22:39 +0200

    efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -22,11 +22,13 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    min_coins = [float('inf')] * (total + 1)
+    max_value = total + 1
+    min_coins = [max_value] * (total + 1)
     min_coins[0] = 0
 
-    for coin in coins:
-        for i in range(coin, total + 1):
-            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
 
-    return min_coins[total] if min_coins[total] != float('inf') else -1
+    return min_coins[total] if min_coins[total] != max_value else -1
