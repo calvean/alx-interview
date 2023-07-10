16344607cd04ed9860a648a26f9d3beec988d0dd
calvean <garnetsharara@gmail.com>
2023-07-10 16:29:45 +0200

    efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,14 +21,17 @@ def makeChange(coins, total):
 
     if total <= 0:
         return 0
-
-    max_value = total + 1
-    min_coins = [max_value] * (total + 1)
-    min_coins[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
-
-    return min_coins[total] if min_coins[total] != max_value else -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
